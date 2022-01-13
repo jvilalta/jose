ae1c4394b9f5293936437690d87d754484c072e8
Jay Vilalta <jay.vilalta@gmail.com>
2022-01-12 18:23:43 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jose
 A command line tool for job searching
+
+Written in .Net Core 6, packaged as a single executable. Available for Windows, Mac and Linux.
---
+++ src/jose.cli/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.CommandLine;
+
+var rootCommand = new RootCommand();
+
+rootCommand.SetHandler(() =>
+{
+    Console.WriteLine("Hello, World!");
+});
+var newCommand = new Command("new");
+var newApplicationCommand = new Command("application");
+newApplicationCommand.AddAlias("app");
+newApplicationCommand.SetHandler(() => {
+    Console.WriteLine("application");
+});
+newCommand.AddCommand(newApplicationCommand);
+
+newCommand.SetHandler(() => {
+    Console.WriteLine("new");
+});
+
+rootCommand.AddCommand(newCommand);
+rootCommand.Invoke(args);
